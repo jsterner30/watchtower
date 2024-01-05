c6e8ffe0a5925580a7758cc7cb754e279eea6cc5
Jaden Sterner <92942612+jsterner30@users.noreply.github.com>
2024-01-05 12:09:24 -0700

    fix: linting

--- src/reports/devPrdBranchesReport.ts
+++ src/reports/devPrdBranchesReport.ts
@@ -1,42 +1,41 @@
 import {
-    type RepoInfo,
-    type ReportFunction
+  type RepoInfo,
+  type ReportFunction
 } from '../types'
 import ReportDataWriter from '../util/reportDataWriter'
 import { errorHandler } from '../util'
 
 export const devPrdBranchesReport: ReportFunction = async (repos: RepoInfo[]): Promise<void> => {
-    const header = [
-        { id: 'repoName', title: 'Repo' },
-        { id: 'hasDev', title: 'Has a Dev Branch' },
-        { id: 'hasPrd', title: 'Has a Prd Branch' },
-        { id: 'devDefault', title: 'Dev Branch is Default' },
-    ]
-    const devPrdBranchesReportWriter = new ReportDataWriter('./data/reports/devPrdBranchReport.csv', header)
+  const header = [
+    { id: 'repoName', title: 'Repo' },
+    { id: 'hasDev', title: 'Has a Dev Branch' },
+    { id: 'hasPrd', title: 'Has a Prd Branch' },
+    { id: 'devDefault', title: 'Dev Branch is Default' }
+  ]
+  const devPrdBranchesReportWriter = new ReportDataWriter('./data/reports/devPrdBranchReport.csv', header)
 
-    for (const repo of repos) {
-        let hasDev = false
-        let hasPrd = false
-        for (const branchName in repo.branches) {
-            try {
-                if (branchName === 'dev') {
-                    hasDev = true
-                }
-                else if (branchName === 'prd') {
-                    hasPrd = true
-                }
-            } catch (error) {
-                errorHandler(error, devPrdBranchesReport.name, repo.name, branchName)
-            }
+  for (const repo of repos) {
+    let hasDev = false
+    let hasPrd = false
+    for (const branchName in repo.branches) {
+      try {
+        if (branchName === 'dev') {
+          hasDev = true
+        } else if (branchName === 'prd') {
+          hasPrd = true
         }
-
-        devPrdBranchesReportWriter.data.push({
-            repoName: repo.name,
-            hasDev,
-            hasPrd,
-            devDefault: repo.defaultBranch === 'dev'
-        })
+      } catch (error) {
+        errorHandler(error, devPrdBranchesReport.name, repo.name, branchName)
+      }
     }
 
-    await devPrdBranchesReportWriter.write()
+    devPrdBranchesReportWriter.data.push({
+      repoName: repo.name,
+      hasDev,
+      hasPrd,
+      devDefault: repo.defaultBranch === 'dev'
+    })
+  }
+
+  await devPrdBranchesReportWriter.write()
 }
