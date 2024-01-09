961bb87166a6688f33902940f4029f26e46f3611
Jaden Sterner <92942612+jsterner30@users.noreply.github.com>
2024-01-09 14:17:46 -0700

    feat: add language ext report

--- src/reports/fileTypesReport.ts
+++ src/reports/fileTypesReport.ts
@@ -1,54 +1,51 @@
-// TODO: THIS REPORT
+import {
+    type RepoInfo,
+    type ReportFunction
+} from '../types'
+import { errorHandler, extensionLanguageMap } from '../util'
+import ReportDataWriter from '../util/reportDataWriter'
 
-// import {
-//     GradeEnum,
-//     type RepoInfo,
-//     type ReportFunction
-// } from '../types'
-// import { errorHandler } from '../util'
-// import ReportDataWriter from '../util/reportDataWriter'
-// import { lowFilesReportGradeName, lowFilesReportGradeWeight } from '../util/constants'
-//
-// export const lowFilesReport: ReportFunction = async (repos: RepoInfo[]): Promise<void> => {
-//     const lowFileRepoWriter = new ReportDataWriter('./data/reports/LowFileCountInRepoReport.csv',
-//         [{ id: 'repoName', title: 'Repo' }, { id: 'fileCount', title: 'FileCount' }])
-//
-//     const lowFileBranchWriter = new ReportDataWriter('./data/reports/LowFileCountOnBranchReport.csv',
-//         [{ id: 'repoName', title: 'Repo' }, { id: 'branchName', title: 'Branch' }, { id: 'fileCount', title: 'FileCount' }])
-//
-//     for (const repo of repos) {
-//         repo.healthScores[lowFilesReportGradeName] = {
-//             grade: GradeEnum.A,
-//             weight: lowFilesReportGradeWeight
-//         }
-//         let someBranchHasFiles = false
-//         for (const branchName in repo.branches) {
-//             try {
-//                 if (repo.branches[branchName].fileCount < 5 && !repo.branches[branchName].dependabot) {
-//                     lowFileBranchWriter.data.push({
-//                         repoName: repo.name,
-//                         branchName,
-//                         fileCount: repo.branches[branchName].fileCount
-//                     })
-//                 } else {
-//                     someBranchHasFiles = true
-//                 }
-//             } catch (error) {
-//                 errorHandler(error, lowFilesReport.name, repo.name, branchName)
-//             }
-//         }
-//         if (!someBranchHasFiles && Object.keys(repo.branches).length !== 0) {
-//             repo.healthScores[lowFilesReportGradeName] = {
-//                 grade: GradeEnum.F,
-//                 weight: lowFilesReportGradeWeight
-//             }
-//             lowFileRepoWriter.data.push({
-//                 repoName: repo.name,
-//                 fileCount: repo.branches[repo.defaultBranch].fileCount
-//             })
-//         }
-//     }
-//
-//     await lowFileBranchWriter.write()
-//     await lowFileRepoWriter.write()
-// }
+export const fileTypesReport: ReportFunction = async (repos: RepoInfo[]): Promise<void> => {
+    const repoHasLanguageReportWriter = new ReportDataWriter('./data/reports/Languages/RepoHasLanguageReport.csv',
+        [{ id: 'repoName', title: 'Repo' }, { id: 'languages', title: 'Languages Found in Repo' }])
+
+    const defaultBranchFileTypeReportWriter = new ReportDataWriter('./data/reports/DefaultBranchFileTypesReport.csv',
+        [{ id: 'repoName', title: 'Repo' }, { id: 'percentageJson', title: 'Percentages' }])
+
+
+    const fileExtensionMap = extensionLanguageMap()
+    for (const repo of repos) {
+        try {
+            const repoLanguageList = new Set<string>
+            for (const branchName in repo.branches) {
+                for (const extension in repo.branches[branchName].fileTypes) {
+                    if (fileExtensionMap[extension] != null) {
+                        repoLanguageList.add(fileExtensionMap[extension])
+                    }
+                }
+
+                if (repo.branches[branchName].defaultBranch) {
+                    let totalFiles = 0
+                    const percentageJson: Record<string, number> = {}
+                    for (const extension in repo.branches[branchName].fileTypes) {
+                        totalFiles += repo.branches[branchName].fileTypes[extension]
+                    }
+                    for (const extension in repo.branches[branchName].fileTypes) {
+                        percentageJson[extension] = repo.branches[branchName].fileTypes[extension] / totalFiles
+                    }
+
+                    defaultBranchFileTypeReportWriter.data.push({'repoName': repo, percentageJson})
+                }
+            }
+
+            repoHasLanguageReportWriter.data.push({'repoName': repo, languages: Array.from(repoLanguageList)})
+
+        } catch (error) {
+            errorHandler(error, fileTypesReport.name, repo.name)
+        }
+    }
+
+    await repoHasLanguageReportWriter.write()
+    await defaultBranchFileTypeReportWriter.write()
+}
+
