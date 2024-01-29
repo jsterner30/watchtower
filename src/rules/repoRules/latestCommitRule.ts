267eacd9d98c302d79de596526d03393ab9a029e
Jaden Sterner <92942612+jsterner30@users.noreply.github.com>
2024-01-29 15:25:31 -0700

    fix: linting

--- src/rules/repoRules/latestCommitRule.ts
+++ src/rules/repoRules/latestCommitRule.ts
@@ -12,7 +12,7 @@ export class LatestCommitRule extends RepoRule {
       })
       let oldestUnfilteredCommit = commits[0]
       for (const commit of commits) {
-        if (!commit.commit.message?.includes('repo-meta') && !commit.commit.message?.includes('repo_meta') && !commit.commit.author?.name?.toLowerCase().includes('service account')) { // filter out non-meaningful commits
+        if (!(commit.commit.message?.includes('repo-meta') || commit.commit.message?.includes('repo_meta')) && !(commit.commit?.author?.name?.toLowerCase()?.includes('service account') ?? false)) { // filter out non-meaningful commits
           oldestUnfilteredCommit = commit
           break
         }
