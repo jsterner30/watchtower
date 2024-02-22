e9046102d8e98a48b839fbcf1b4e3ab7ec3cca48
Jaden Sterner <92942612+jsterner30@users.noreply.github.com>
2024-02-22 16:27:23 -0700

    fix: report descript

--- src/reports/getOverallReports.ts
+++ src/reports/getOverallReports.ts
@@ -12,6 +12,6 @@ export function getOverallReports (): OverallReports {
   return {
     overallBranchReport: new OverallBranchReport(0, 'OverallReports', 'General details about every non-dependabot branch in the org', '1', 'overall'),
     overallRepoReport: new OverallRepoReport(0, 'OverallReports', 'General details about the non archived repo in the org', '1', 'overall'),
-    overallHealthScoreReport: new OverallHealthScoreReport(0, 'OverallReports', 'A healthscore assigned to each repo based on contributing repo reports. More info can be found in the "Overall Health Scoring" section of the README.md file.', '1', 'overall')
+    overallHealthScoreReport: new OverallHealthScoreReport(0, 'OverallReports', 'A healthscore is assigned to each repo based on contributing repo reports. More info can be found in the "Overall Health Scoring" section of the README.md file.', '1', 'overall')
   }
 }
