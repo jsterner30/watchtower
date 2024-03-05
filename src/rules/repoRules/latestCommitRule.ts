a5b5c7e069e4df1a35d39e3bf0ed26b90152ad8e
Jaden Sterner <92942612+jsterner30@users.noreply.github.com>
2024-03-05 16:02:30 -0700

    fix: latestCommitRule

--- src/rules/repoRules/latestCommitRule.ts
+++ src/rules/repoRules/latestCommitRule.ts
@@ -6,7 +6,7 @@ export class LatestCommitRule extends RepoRule {
   async run (repo: Repo): Promise<void> {
     try {
       const commits = await getCommits(repo.name)
-      let oldestUnfilteredCommit = commits[0]
+      let newestUnfilteredCommit = commits[0]
       for (const commit of commits) {
         if (!(commit.message.toLowerCase().includes('repo-meta') || commit.message.toLowerCase().includes('repo_meta')) && // exclude repo-meta commits
             !(commit.author.toLowerCase().includes('service account') ?? false) && // exclude service account commits
@@ -14,15 +14,13 @@ export class LatestCommitRule extends RepoRule {
             !(commit.message.toLowerCase().includes('maintained_by')) && // exclude changes to repo-meta file maintained_by commits
             !(commit.message.toLowerCase().includes('richardskg/patch-1')) // exclude the merged PRs of changes to repo-meta file maintained_by commits
         ) { // filter out non-meaningful commits
-          oldestUnfilteredCommit = commit
+          newestUnfilteredCommit = commit
           break
         }
       }
 
       if (commits.length > 0) {
-        repo.lastCommit.date = oldestUnfilteredCommit.date
-        repo.lastCommit.author = oldestUnfilteredCommit.author
-        repo.lastCommit.message = oldestUnfilteredCommit.message
+        repo.lastCommit = newestUnfilteredCommit
       }
     } catch (error) {
       errorHandler(error, LatestCommitRule.name, repo.name)
